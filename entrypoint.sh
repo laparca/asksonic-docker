5d9942e7924a49f75899a90736408d53a6ea56bd
Samuel R. Sevilla <laparca@laparca.es>
2022-04-26 13:56:49 +0000

    feat: try to improve debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,8 @@ export PATH="$PATH:/asksonic/deps/bin"
 PORT=8080
 cd asksonic
 
-gunicorn asksonic:app --bind "${ASKS_HOST:-0.0.0.0}:${ASKS_PORT:-$PORT}" --workers 1
+if [ "${FLASK_ENV}" == development ]; then
+    python server.py
+else
+    gunicorn asksonic:app --bind "${ASKS_HOST:-0.0.0.0}:${ASKS_PORT:-$PORT}" --workers 1
+fi
